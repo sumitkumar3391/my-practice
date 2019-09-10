af53ee8f91849d94b491c91183e3bd90dd5ef5f6
sumit__kumar <sumit@gmail.com>
2019-09-10 05:54:24 +0530

    Initial Commit

---
+++ MyPractice/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
