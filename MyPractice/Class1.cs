16ec4e40ecc143053eb612a73de4fe535e4f5828
sumit__kumar <sumit@gmail.com>
2019-09-10 07:31:21 +0530

    modified changes

---
+++ MyPractice/Class1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace MyPractice
+{
+    class Class1
+    {
+        public int Multi()
+        {
+            Console.WriteLine("enter Count of Number ");
+            string count = Console.ReadLine();
+            int count1 = Convert.ToInt32(count);
+            int result = 1;
+            for (int i = 0; i < count1; i++)
+            {
+                
+                Console.WriteLine("enter value ");
+                string value12 = Console.ReadLine();
+                int value121 = Convert.ToInt32(value12);
+                result = value121 * result;
+
+            }
+           
+           
+            Console.WriteLine("result is");
+            return result;
+           
+           
+        }
+
+
+        public void Read(string textFile)
+        {
+            Console.WriteLine("enter the Directory name ");
+            string dir = Console.ReadLine();
+            string path1 = @"C:\\Users";
+            Console.WriteLine("Enter user name");
+            string path2 = Console.ReadLine();
+            
+            string combination = Path.Combine(path1, path2, dir);
+            Directory.SetCurrentDirectory(combination);
+            
+            Console.WriteLine(combination);
+           
+            if (File.Exists(textFile))
+            {
+                Console.WriteLine("file Exists");
+                string text3 = File.ReadAllText(textFile);
+                Console.WriteLine(text3);
+            }
+            else
+            {
+                Console.WriteLine("file doesnot Exists");
+            }
+        }
+        public List<string> Iteration()
+        {
+            List<string> ls = new List<string>(10);
+            ls.Add("one");
+            ls.Add("two");
+            ls.Add("three");
+            ls.Add("four");
+
+            return ls;
+        }
+    }
+
+}
--- MyPractice/Program.cs
+++ MyPractice/Program.cs
@@ -10,6 +10,84 @@ namespace MyPractice
     {
         static void Main(string[] args)
         {
+            Class1 m1 = new Class1();
+            bool exit = true;
+            do
+
+            {
+                Console.WriteLine("1:For Multiplication");
+                Console.WriteLine("2:For reading Text File");
+                Console.WriteLine("3:for reading properties of a c# object");
+                Console.WriteLine("4:For Exit");
+                string input = Console.ReadLine();
+                int value3 = Convert.ToInt32(input);
+
+                switch (value3)
+                {
+                    case 1:
+                        int result2 = m1.Multi();
+                        Console.WriteLine(result2);
+                        Console.ReadLine();
+                        break;
+                    case 2:
+                        Console.WriteLine("ENTER TEXT FILE NAME");
+                        string filename = Console.ReadLine();
+
+                        m1.Read(filename);
+                        Console.ReadLine();
+                        break;
+                    case 3:
+                        Console.WriteLine("listIteration  is ");
+                        List<string> range = m1.Iteration();
+                        foreach (string item in range)
+                        {
+                            Console.WriteLine(item);
+                        }
+                        Console.ReadLine();
+                        break;
+                    case 4:
+                        Console.WriteLine("Bye Have a Good Day");
+                        Console.ReadLine();
+                        exit = false;
+                        break;
+
+
+                }
+
+
+
+
+
+
+
+
+
+
+
+
+            } while (exit);
+
+
+
+
+
         }
     }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+    } 
+
+
+
+
+
